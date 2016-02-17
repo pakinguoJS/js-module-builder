cb54249108f91039d6653b46f9a9ef6c83d3b0ab
pakinguoJS <pakinguo@qq.com>
2016-02-17 10:12:42 +0800

    update

--- bin/init.sh
+++ bin/init.sh
@@ -3,6 +3,6 @@
 chmod 755 jsmb jsmb-watch-start.sh jsmb-watch-stop.sh jsmb-restart.sh
 
 ln -s /home/pakinguo/jsmb/bin/jsmb /usr/local/bin/jsmb
-ln -s /home/pakinguo/jsmb/bin/jsmb-watch-start.sh /usr/local/bin/jsmb-start
+ln -s /home/pakinguo/jsmb/bin/jsmb-restart.sh /usr/local/bin/jsmb-start
 ln -s /home/pakinguo/jsmb/bin/jsmb-watch-stop.sh /usr/local/bin/jsmb-stop
 ln -s /home/pakinguo/jsmb/bin/jsmb-restart.sh /usr/local/bin/jsmb-restart
